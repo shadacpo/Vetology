c93fffb79d6bc44cf446aaaa4d99ba50bbf8e709
shadacpo <shoshoomar28282@gmail.com>
2026-03-29 00:59:47 +0200

    Add tailwind.config.js with custom theme configuration for colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#FBBF24',
+        accent: '#34D399',
+        neutral: '#3D3D3D',
+        'base-100': '#FFFFFF',
+        'info': '#3ABFF8',
+        'success': '#36D399',
+        'warning': '#FBBD23',
+        'error': '#F87272',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
